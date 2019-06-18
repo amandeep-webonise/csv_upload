bb342154a3da030207536b4d31e45b4183729eb1
amandeep <amandeep@webonise.com>
2019-06-18 14:36:35 +0530

    handle some error

--- app/controllers/upload_controller.go
+++ app/controllers/upload_controller.go
@@ -36,23 +36,27 @@ func (s *Srv) UploadFile(w *framework.Response, r *framework.Request) {
 	}
 	defer file.Close()
 	//Extracting the extension of file
-	fslice := strings.Split(handler.Filename, ".")
-	fileExtension := fslice[len(fslice)-1]
+	//fslice := strings.Split(handler.Filename, ".")
+	//fileExtension := fslice[len(fslice)-1]
 	//Handling CSV and XLSX file extension
-	switch fileExtension {
-	case "csv": //Handle file with extension CSV
+	switch {
+	case strings.HasSuffix(handler.Filename, "csv"): //Handle file with extension CSV
 		reader := csv.NewReader(file)
-		record, _ := reader.ReadAll()
-		for _, line := range record[1:] {
-			err := s.Service.CheckEmployeesExist(line)
-			if err != nil {
-				//fmt.Println("Error while processing CSV extension", err)
-				returnMessage = "Error while processing CSV extension"
+		record, readErr := reader.ReadAll()
+		if readErr != nil {
+			returnMessage = "Error while reading CSV file"
+		} else {
+			for _, line := range record[1:] {
+				err := s.Service.CheckEmployeesExist(line)
+				if err != nil {
+					//fmt.Println("Error while processing CSV extension", err)
+					returnMessage = "Error while processing CSV extension"
+				}
 			}
 		}
 		defer file.Close()
 
-	case "xlsx": //Handle file with extension XLSX
+	case strings.HasSuffix(handler.Filename, "xlsx"): //Handle file with extension XLSX
 		f, err := os.OpenFile("./web/assets/files/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
 		if err != nil {
 			//fmt.Println("Error while trying to open a file", err)
@@ -62,32 +66,35 @@ func (s *Srv) UploadFile(w *framework.Response, r *framework.Request) {
 		//Copy the file content
 		io.Copy(f, file)
 
-		xlFile, _ := xlsx.OpenFile("./web/assets/files/" + handler.Filename)
-
-		for _, sheet := range xlFile.Sheets {
-			//ignore first row because it contain Header
-			for _, row := range sheet.Rows[1:] {
-				emp := make([]string, 0)
-				for _, cell := range row.Cells {
-					text := cell.String()
-					emp = append(emp, text)
-				}
-				err := s.Service.CheckEmployeesExist(emp)
-				if err != nil {
-					//fmt.Println("Error while processing XLSX extension", err)
-					returnMessage = "Error while processing XLSX extension"
+		xlFile, openFileErr := xlsx.OpenFile("./web/assets/files/" + handler.Filename)
+		if openFileErr != nil {
+			returnMessage = "Error while reading XLSX file"
+		} else {
+			for _, sheet := range xlFile.Sheets {
+				//ignore first row because it contain Header
+				for _, row := range sheet.Rows[1:] {
+					emp := make([]string, 0)
+					for _, cell := range row.Cells {
+						text := cell.String()
+						emp = append(emp, text)
+					}
+					err := s.Service.CheckEmployeesExist(emp)
+					if err != nil {
+						//fmt.Println("Error while processing XLSX extension", err)
+						returnMessage = "Error while processing XLSX extension"
+					}
 				}
 			}
 		}
 
 	default:
 		//if extension not supported
-		returnMessage = "Upload file with " + fileExtension + " extension not allowed"
+		returnMessage = "Upload file extension not supported"
 	}
 
 	returnStatus := false
 	if returnMessage == "" {
-		returnMessage = "File uploaded and information update sucessfully"
+		returnMessage = "File uploaded and information updated sucessfully"
 		returnStatus = true
 
 	}
